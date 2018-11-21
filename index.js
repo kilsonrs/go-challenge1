ee3f27dad6ac057dde4e2d340946581a8cb96453
Kilson <kilsonrs@gmail.com>
2018-11-21 10:57:41 -0200

    Iniciando desafio 1

---
+++ index.js
@@ -0,0 +1,31 @@
+// const http = require('nome-da-biblioteca');
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+
+const users = ['Kilson Santos', 'Robson Marques', 'Diego Fernandes']
+
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
+
+app.listen(3000)
